315e9f2c4e31ba885d698bec842fc104f9d16796
anitha <dinesh.rays@gmail.com>
2018-09-05 10:52:09 +0530

    ANI: Updated modified code

---
+++ JobPortal/ViewModel/JobLocDetViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JobPortal.Models;
+
+namespace JobPortal.ViewModel
+{
+    public class JobLocDetViewModel
+    {
+        public int Id { get; set; }
+        public int JobPostId { get; set; }
+        public int CityID { get; set; }
+        public Nullable<bool> Isactive { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ JobPortal/ViewModel/JobSkillDetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JobPortal.Models;
+
+namespace JobPortal.ViewModel
+{
+    public class JobSkillDetViewModel
+    {
+
+        public string SkillName { get; set; }
+        public int Id { get; set; }
+        public int SkillId { get; set; }
+        public int JobPostId { get; set; }
+        public Nullable<bool> Isactive { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+    }
+}
